641926faf2a6da4dae56ef0e3eb58acb41bc4748
sean.spinney <sean.spinney@server.mila.quebec>
2022-01-23 20:00:38 -0500

    Fixed some settings for mnist

--- final_configs/cifar10_DDTPConv.py
+++ final_configs/cifar10_DDTPConv.py
@@ -3,13 +3,13 @@ config = {
 'beta1_fb': 0.9,
 'beta2': 0.999,
 'beta2_fb': 0.999,
-'epsilon': '[2.7867895625009e-08, 1.9868935703787622e-08, 4.515242618159344e-06, 4.046144976139705e-05]',
+'epsilon': [2.7867895625009e-08, 1.9868935703787622e-08, 4.515242618159344e-06, 4.046144976139705e-05],
 'epsilon_fb': 7.529093372180766e-07,
 'feedback_wd': 6.169295107849636e-05,
 'lr': [0.00025935571806476586, 0.000885500279951265, 0.0001423047695105589, 3.3871035558126015e-06],
 'lr_fb': 0.0045157498494467095,
 'nb_feedback_iterations': [1, 1, 1, 1],
-'sigma': 0.00921040366516759,
+'sigma': [0.00921040366516759,0.00921040366516759,0.00921040366516759,0.00921040366516759],
 'target_stepsize': 0.015962099947441903,
 'dataset': 'cifar10',
 #'out_dir': 'logs/DDTPConv_cifarCIFAR',
@@ -20,7 +20,7 @@ config = {
 # ### Training options ###
 'optimizer': 'Adam',
 'optimizer_fb': 'Adam',
-'momentum': 0.,
+'momentum': 0.9,
 'parallel': True,
 'normalize_lr': True,
 'batch_size': 128,
@@ -31,6 +31,7 @@ config = {
 'extra_fb_epochs': 1,
 'epochs': 90,
 'double_precision': True,
+'scheduler': True,
 
 ### Network options ###
 # 'num_hidden': 3,
